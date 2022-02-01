3ccf499b5fe18fe06d469f147c54a0d993c2231d
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2022-02-01 14:15:21 +0100

    Add Controls Update

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import Controls from "../Templates/Controls";
+
+const Home = () => {
+    return ( 
+        <div className="">
+            <Controls />
+        </div>
+     );
+}
+
+export default Home;
