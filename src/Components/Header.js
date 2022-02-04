1276a0093d42d167b420e78dfbc3d1a3d59af55e
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2022-02-04 08:06:54 +0100

    Add Header Update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,11 @@
+import logo from '../Ellipse 20.png';
+
+
 const Header = () => {
     return ( 
-        <div className="text-white text-4xl font-bold">
-            Control <br /> Panel
+        <div className="text-white text-4xl font-bold h-40 flex content-center justify-between">
+            <h2 className="ml-6 mt-12">Control <br /> Panel</h2>
+            <img src={logo} className='scale-105 self-center mt-6 mr-3'></img>
         </div>
      );
 }
