a85d1645c07ff9a9332a9ae6be082431e66f8c8f
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2022-02-04 14:03:00 +0100

    Add Change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,16 @@
 import logo from '../Ellipse 20.png';
+import { Link } from 'react-router-dom';
+var ControlText = "Control";
 
 
 const Header = () => {
     return ( 
         <div className="text-white text-4xl font-bold h-40 flex content-center justify-between overflow-hidden relative">
-            <h2 className="ml-6 mt-12">Control <br /> Panel</h2>
-            <div className='h-40 bg-white/5 rounded-full w-40 absolute -mt-20 -ml-10 z-0'></div>
-            <div className='h-40 bg-white/5 rounded-full w-40 absolute mt-36 ml-8 z-0'></div>
+            <Link to="/" className="text-3xl ml-6 mt-16">{ControlText} <br /> Panel</Link>
+            <div className='h-40 bg-white/5 rounded-full w-40 absolute -mt-20 -ml-10 z-0 overflow-hidden'></div>
+            <div className='h-40 bg-white/5 rounded-full w-40 absolute mt-36 ml-8 z-0 overflow-hidden'></div>
             <div className=''>
-                <div className='h-40 bg-white/5 rounded-full w-40 absolute mt-12 ml-16 z-0'></div>
+                <div className='h-40 bg-white/5 rounded-full w-40 absolute mt-12 ml-20 z-0'></div>
             </div>
             <div className='self-center'>
                 <div className='h-3 bg-red-400 rounded-full w-3 absolute mt-10 ml-14 z-10'></div>
